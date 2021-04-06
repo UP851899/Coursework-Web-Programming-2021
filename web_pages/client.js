b33b31ccab6b0cb3c137de3844d2b69b60ee6782
UP851899 <72556275+UP851899@users.noreply.github.com>
2021-04-06 10:30:10 +0100

    Added drag and drop

---
+++ web_pages/client.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-unused-vars */
+function dropHandler(e) {
+  console.log('File(s) have been dropped');
+
+  // Prevent browsers default behaviour where it opens the file
+  e.preventDefault();
+
+  if (e.dataTransfer.items) {
+    // Access uploaded files (DataTransferItemList method)
+    for (let i = 0; i < e.dataTransfer.items.length; i++) {
+      if (e.dataTransfer.items[i].kind === 'file') {
+        const file = e.dataTransfer.items[i].getAsFile();
+        console.log('... file[' + i + '].name = ' + file.name);
+      }
+    }
+  } else {
+    // Access uploaded files (DataTransfer method)
+    for (let i = 0; i < e.dataTransfer.items.length; i++) {
+      console.log('... file[' + i + '].name = ' + e.dataTransfer.files[i].name);
+    }
+  }
+}
+
+function dragOverHandler(e) {
+  console.log('File(s) are in the drop zone');
+
+  // Prevents browsers default actions
+  e.preventDefault();
+}
