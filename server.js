a4fa9a770ffecc4426c824bac62d01a4c2a753d7
UP851899 <72556275+UP851899@users.noreply.github.com>
2021-04-06 10:49:53 +0100

    server loads webpages

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
+/* eslint-disable no-unused-vars */
 const express = require('express');
+const config = require('./config.json');
+
 const app = express();
 
 app.listen(8080, (e) => {
   console.log(`server ${e ? 'failed to start' : 'listening'}`);
 });
+
+// Get webpages
+app.use(express.json());
+app.use('/', express.static('web_pages'));
