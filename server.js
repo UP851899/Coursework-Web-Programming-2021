83b2f1877c9046efc97e9520c1da4275935e10cc
UP851899 <72556275+UP851899@users.noreply.github.com>
2021-04-29 15:31:17 +0100

    added sqlite3 setup

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const multer = require('multer');
+const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
 const port = 8080;
+const DB_PATH = ':memory:';
 
 app.listen(port, (e) => {
   console.log(`server ${e ? 'failed to start' : `listening on port ${port}`}`);
+});
+
+// Database
+// eslint-disable-next-line no-unused-vars
+const DB = new sqlite3.Database(DB_PATH, function (err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('Connected to database: ' + DB_PATH);
 });
 
 // Get webpages
