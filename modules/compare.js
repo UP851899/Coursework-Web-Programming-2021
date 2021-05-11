ef8c8ce2ccbcb88764461be0125d4629488a0032
UP851899 <72556275+UP851899@users.noreply.github.com>
2021-05-11 16:54:08 +0100

    added file comparison script

---
+++ modules/compare.js
@@ -0,0 +1,56 @@
+'use strict';
+/* eslint-disable prefer-const */
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+// const fetch = require('node-fetch');
+const fs = require('fs');
+const stringSimilarity = require('string-similarity');
+
+// Test run \\
+export function compareTest() {
+  const fileOne = fs.readFileSync('.\\files\\Test.txt', 'utf8', function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('file read');
+      return data;
+    }
+  });
+
+  const fileTwo = fs.readFileSync('.\\files\\Test2.txt', 'utf8', function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('file read');
+      return data;
+    }
+  });
+
+  const testValue = stringSimilarity.compareTwoStrings(fileOne, fileTwo);
+  return testValue;
+}
+
+export function comparison(pathOne, pathTwo) {
+  const test1 = fs.readFileSync('.\\' + pathOne, 'utf8', function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('file1 read at - ' + pathOne);
+      return data;
+    }
+  });
+
+  const test2 = fs.readFileSync('.\\' + pathTwo, 'utf8', function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('file2 read at - ' + pathTwo);
+      return data;
+    }
+  });
+
+  const testValue = stringSimilarity.compareTwoStrings(test1, test2);
+  console.log('Percentage = ' + testValue);
+  return testValue;
+}
